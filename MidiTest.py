2c3c9be6b5c4b1642bf9b0e88bb469e9454bb1ea
jamin-hu <38098193+jamin-hu@users.noreply.github.com>
2019-03-17 17:45:26 +0200

    Tested Virtual Midi Out

--- MidiTest.py
+++ MidiTest.py
@@ -1,9 +1,15 @@
 import mido
+from mido import Message
 
-inports = mido.get_input_names()
+outports = mido.get_output_names()
+index = [i for i, s in enumerate(outports) if 'VirtualMidiPort1' in s]
 
-index = [i for i, s in enumerate(inports) if 'VirtualMidiPort1' in s]
+testMsg = Message('note_on', note=60)
 
-inport = mido.open_input(inports[index[0]])
+outport = mido.open_output(outports[index[0]])
+outport.send(testMsg)
 
-print('Inport = ' + inport.name)
+# with mido.open_input() as inport:
+#     print('Inport = ' + inport.name)
+#     for msg in inport:
+#         print(msg)
