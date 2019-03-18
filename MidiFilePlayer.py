bc37d1e2cc34573ecb9b30ec18027521d3d0e18e
jamin-hu <38098193+jamin-hu@users.noreply.github.com>
2019-03-18 16:02:51 +0200

    Connected Ploytec MIDI Cable

--- MidiFilePlayer.py
+++ MidiFilePlayer.py
@@ -5,7 +5,7 @@ from mido import MidiFile
 filePath = 'MidiRecordings/Chopin/chpn_op10_e12.mid'
 
 outports = mido.get_output_names()
-index = [i for i, s in enumerate(outports) if 'VirtualMidiPort1' in s]
+index = [i for i, s in enumerate(outports) if 'VirtualMidiOut' in s]
 outport = mido.open_output(outports[index[0]])
 
 for msg in MidiFile(filePath).play():
--- MidiPortFlipper.py
+++ MidiPortFlipper.py
@@ -3,12 +3,12 @@ from mido import Message
 from mido import MidiFile
 
 outports = mido.get_output_names()
-outIndex = [i for i, s in enumerate(outports) if 'VirtualMidiOut' in s]
+outIndex = [i for i, s in enumerate(outports) if 'VirtualMidiOut' in s] #Name of virtual MIDI output cable
 outport = mido.open_output(outports[outIndex[0]])
 print("Outport = " + str(outport.name))
 
 inports = mido.get_input_names()
-inIndex = [j for j, t in enumerate(inports) if 'VirtualMidiIn' in t]
+inIndex = [j for j, t in enumerate(inports) if 'Ploytec MIDI Cable' in t] #Name of MIDI input cable
 
 with mido.open_input(inports[inIndex[0]]) as inport:
     print("Inport = " + str(inport.name))
