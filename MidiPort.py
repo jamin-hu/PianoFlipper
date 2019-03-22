7adf46423e563a7805b66d9f048ab27909758f16
jamin-hu <38098193+jamin-hu@users.noreply.github.com>
2019-03-22 22:47:39 +0200

    Replaced _thread implementation with pynput

--- MidiPort.py
+++ MidiPort.py
@@ -3,54 +3,44 @@ from mido import Message
 from mido import MidiFile
 
 import _thread, time
-import threading
+from pynput import keyboard
+import time
+import sys
 
-def input_thread(L, flipped):
-    derp = input()
-    if flipped == True:
-        print("Flipping mode deactivated")
-    elif flipped == False:
-        print("Flipping mode activated")
-    L.append(derp)
+active = True
 
+def on_press(key):
+    global active
+    print(key.char)
+    if active == True:
+        print("Normal")
+        active = False
+    elif active == False:
+        print("FLIPPED!")
+        active = True
 
-def do_print():
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
-    outports = mido.get_output_names()
-    outIndex = [i for i, s in enumerate(outports) if 'VirtualMidiOut' in s] #Name of virtual MIDI output cable
-    outport = mido.open_output(outports[outIndex[0]])
-    print("Outport = " + str(outport.name))
+outports = mido.get_output_names()
+outIndex = [i for i, s in enumerate(outports) if 'VirtualMidiOut' in s] #Name of virtual MIDI output cable
+outport = mido.open_output(outports[outIndex[0]])
+print("Outport = " + str(outport.name))
 
-    inports = mido.get_input_names()
-    inIndex = [j for j, t in enumerate(inports) if 'VirtualMidiIn' in t] #Name of MIDI input cable
+inports = mido.get_input_names()
+inIndex = [j for j, t in enumerate(inports) if 'VirtualMidiIn' in t] #Name of MIDI input cable
 
-    with mido.open_input(inports[inIndex[0]]) as inport:
-        L = []
-        flipped = False
-        _thread.start_new_thread(input_thread, (L, flipped,))
-        #print(_thread.get_ident())
-
-        print("Inport = " + str(inport.name))
-        for msg in inport:
-
-            if L:
-                del(L[0])
-                if flipped == True:
-                    flipped = False
-                elif flipped == False:
-                    flipped = True
-                _thread.start_new_thread(input_thread, (L,flipped,))
-
-            if flipped == True:
-                try:
-                    note= msg.note
-                    flippedMsg = msg.copy(note=127-note)
-                    print(flippedMsg)
-                    outport.send(flippedMsg)
-                except:
-                    pass
-
-            if flipped == False:
-                print(msg)
-                outport.send(msg)
-do_print()
+with mido.open_input(inports[inIndex[0]]) as inport:
+    print("Inport = " + str(inport.name))
+    for msg in inport:
+        if active == True:
+            try:
+                note= msg.note
+                flippedMsg = msg.copy(note=127-note)
+                print(flippedMsg)
+                outport.send(flippedMsg)
+            except:
+                pass
+        else:
+            print(msg)
+            outport.send(msg)
